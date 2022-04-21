393354d2470084f7637b2cda5b47f2638da2ee27
zhangzeyu007 <zhangzeyu1234@gmail.com>
2022-04-21 13:40:54 +0800

    feat: vite

--- unlimitedServer/config/plugin.ts
+++ unlimitedServer/config/plugin.ts
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Author: 张泽雨
+ * @Date: 2022-04-12 12:48:36
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2022-04-21 11:59:41
+ * @FilePath: \unlimitedServer\config\plugin.ts
+ */
 import { EggPlugin } from 'egg';
 
 const plugin: EggPlugin = {
@@ -9,6 +17,10 @@ const plugin: EggPlugin = {
   sequelize: {
     enable: true,
     package: 'egg-sequelize'
+  },  
+  nunjucks: {
+  enable: true,
+  package: 'egg-view-nunjucks',
   }
 };
 
--- unlimitedfe/vite.config.ts
+++ unlimitedfe/vite.config.ts
@@ -3,17 +3,19 @@
  * @Author: 张泽雨
  * @Date: 2022-04-13 16:23:52
  * @LastEditors: 张泽雨
- * @LastEditTime: 2022-04-15 12:05:55
+ * @LastEditTime: 2022-04-21 13:37:45
  * @FilePath: \unlimitedfe\vite.config.ts
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path';
 // https://vitejs.dev/config/
-export default defineConfig({
+
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '' : '/',
   plugins: [vue()],
   resolve: {
     alias: { '@': resolve(__dirname, './src') },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
   },
-})
+}))
